fda4ecc15cb153e856e274ce39f8f98624b7e2af
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-07-04 20:04:29 -0400

    changing colors

--- stash/codeCamp.js
+++ stash/codeCamp.js
@@ -5,6 +5,7 @@ let turn;
 let good;
 let compTurn;
 let intervalId;
+let strict = false;
 let noise = true;
 let on = false;
 let win;
@@ -14,8 +15,17 @@ const topLeft = document.querySelector('#topleft');
 const topRight = document.querySelector('#topright');
 const bottomLeft = document.querySelector('#bottomleft');
 const bottomRight = document.querySelector('#bottomright');
+const strictButton = document.querySelector('#strict');
 const onButton = document.querySelector('#on');
 const startButton = document.querySelector('#start');
+
+strictButton.addEventListener('click', (event) => {
+	if (strictButton.checked == true) {
+		strict = true;
+	} else {
+		strict = false;
+	}
+});
 
 onButton.addEventListener('click', (event) => {
 	if (onButton.checked == true) {
