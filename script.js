e8894bb0ee162e8f73902183a6dc7fbeb4d619b0
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-08-19 16:48:42 -0400

    not solved yet time delay when not click

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let win;
 let cardId;
 let card;
 let on = false;
+let cardClicked = false;
 
 const turnCounter = document.querySelector('#turn');
 const startButton = document.querySelector('#start');
@@ -79,9 +80,9 @@ function gameTurn() {
 
 	if (compTurn) {
 		clearColor();
-		cards.forEach((card) => {
-			card.removeEventListener('click', (e) => {});
-		});
+		// cards.forEach((card) => {
+		// 	card.removeEventListener('click', (e) => {});
+		// });
 
 		setTimeout(() => {
 			colorPick(cards[order[flash] - 1]);
@@ -112,9 +113,13 @@ function colorPick(card) {
 
 cards.forEach((card) => {
 	card.addEventListener('click', (e) => {
-		cardId = +card.dataset.id;
+		// card.addEventListener('click', (e) => {
+		cardClicked = true;
 
+		// console.log('card is here', card);
 		if (on) {
+			// console.log('hello', 'card', card);
+			cardId = +card.dataset.id;
 			playerOrder.push(cardId);
 			check();
 			colorPick(card);
@@ -128,6 +133,74 @@ cards.forEach((card) => {
 });
 
 function check() {
+	count = 0;
+	console.log(compTurn, cardClicked);
+	// let timer = !compTurn;
+	let timer = cardClicked;
+	if (timer) {
+		count++;
+		timer = window.setTimeout(notClicked, 5000);
+		console.log(count);
+		// if (card.addEventListener) {
+		// 	clicked();
+		// if (timer && card.addEventListener) {
+		// 	clicked();
+		// }
+	}
+	function notClicked() {
+		console.log(count, '151');
+		if (count === 1) {
+			console.log('hi');
+			// if (!card.addEventListener) {
+			// clicked();
+			flashColor();
+			good == false;
+			// turnCounter.innerHTML = 'try again';
+			count++;
+			console.log(count, '159', 'good', good);
+		}
+		if (count > 1) {
+			console.log(count, '162');
+			clicked();
+			count = 0;
+			console.log(count, '165', 'good', good);
+			// setTimeout(() => {
+			// 	turnCounter.innerHTML = turn;
+			// 	clearColor();x
+
+			// 	compTurn = true;
+			// 	flash = 0;
+			// 	playerOrder = [];
+			// 	good = true;
+			// 	intervalId = setInterval(gameTurn, 800);
+			// }, 800);
+		}
+		// good == false;
+	}
+	// }
+	// }
+	// if (card.addEventListener) {
+	// 	clicked();
+	// 	console.log('cleared');
+	// 	// setTimeout(() => {
+	// 	// 	turnCounter.innerHTML = turn;
+	// 	// 	clearColor();
+
+	// 	// 	compTurn = true;
+	// 	// 	flash = 0;
+	// 	// 	playerOrder = [];
+	// 	// 	good = true;
+	// 	// 	intervalId = setInterval(gameTurn, 800);
+	// 	// }, 800);
+	// }
+	// }
+	function clicked() {
+		clearTimeout(timer);
+		console.log('howdy');
+	}
+	// }
+
+	// if (playerOrder[playerOrder.length - 1] !== order[playerOrder.length - 1]) good = false;
 	if (playerOrder[playerOrder.length - 1] !== order[playerOrder.length - 1]) good = false;
 	if (playerOrder.length == 10 && good) {
 		winGame();
@@ -136,6 +209,7 @@ function check() {
 	if (good == false) {
 		flashColor();
 		turnCounter.innerHTML = 'try again';
+		clicked();
 		setTimeout(() => {
 			turnCounter.innerHTML = turn;
 			clearColor();
@@ -147,6 +221,7 @@ function check() {
 			intervalId = setInterval(gameTurn, 800);
 		}, 800);
 	}
+	// }
 
 	if (turn == playerOrder.length && good && !win) {
 		turn++;
